0e92b098165f128ec82e818097d834ee16dca0e5
Kawa Yg <yangjiahua19920809@gmail.com>
2018-02-07 17:02:30 +0800

    XLog replace NSLog

---
+++ UserRegister/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UserRegister
+//
+//  Created by yangjiahua on 2018/2/7.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+//        XLog(@"test");
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
